0566bec8e0e1b45e507ce1e5ac6933dff240fc79
llq360 <30930158+llq360@users.noreply.github.com>
2018-02-09 21:48:44 +0800

    1234

--- statistics.js
+++ statistics.js
@@ -1,3 +1,1 @@
-//CNZZ
-var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-document.write(unescape("%3Cspan id='cnzz_stat_icon_1261063131'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s4.cnzz.com/z_stat.php%3Fid%3D1261063131' type='text/javascript'%3E%3C/script%3E"));
+<script src="https://s4.cnzz.com/z_stat.php?id=123&web_id=123" language="JavaScript"></script>
